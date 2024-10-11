f98d8047f32ed4700cefa993c7d043cda97c589b
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-11 16:52:59 +0600

    post detail page created

--- src/services/PostServices/index.ts
+++ src/services/PostServices/index.ts
@@ -1,5 +1,7 @@
 import { delay } from "@/utils/delay";
 
+
+// getAllPosts function to get all posts 
 export const getAllPosts = async (type?: string, wait = false) => {
   let fetchOptions = {};
 
@@ -19,6 +21,20 @@ export const getAllPosts = async (type?: string, wait = false) => {
     `${process.env.NEXT_PUBLIC_BASE_API}/posts`,
     fetchOptions
   );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  if (wait) {
+    delay(2000);
+  }
+  return res.json();
+};
+
+
+// getPost function to get a single post by id
+export const getPost = async (id: string , wait = false) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/posts/${id}`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
